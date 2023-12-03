647b5f0d688855a555a95deb08ea19bda16d9d1e
Kerem Karaman <keremkaraman91@gmail.com>
2023-12-03 15:28:40 +0300

    book class created

---
+++ src/models/Book.java
@@ -0,0 +1,72 @@
+package models;
+
+import java.util.Objects;
+
+public abstract class Book {
+    private long id;
+    private String title;
+    private String author;
+    private BookCategory bookCategory;
+
+    public Book(long id, String title, String author, BookCategory bookCategory) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.bookCategory = bookCategory;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public BookCategory getBookCategory() {
+        return bookCategory;
+    }
+
+    public void setBookCategory(BookCategory bookCategory) {
+        this.bookCategory = bookCategory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id == book.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", bookCategory=" + bookCategory +
+                '}';
+    }
+}
---
+++ src/models/BookCategory.java
@@ -0,0 +1,13 @@
+package models;
+
+public enum BookCategory {
+    FANTASY,
+    TRANSGRESSIONAL,
+    FICTION,
+    CLASSICS,
+    TRAGEDIES,
+    PSYCHOLOGICAL_FICTION,
+    CRIME_FICTION
+
+
+}
